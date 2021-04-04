abe8d91706daa01d2a1a75c5f8ddc7af99b2ff15
kindeme <fkounoudji@gmail.com>
2021-04-03 23:21:55 -0500

    all  methods completed

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -7,23 +7,31 @@ namespace Homework2
         static void Main(string[] args)
         {
             int[] numbers = new int[] { 10, 5, 9, 87, 56 };
-            //SortArray(numbers);
-            //AddElementAtStart(numbers, 12);
-            // AddElementAtEnd(numbers,35);
-            // AddElementAtAnyPosition(numbers, 33, 4);
-            //RemoveElementAtStart(numbers);
-            //RemoveElementAtEnd(numbers);
-            // RemoveElementAtPositon(numbers, 3);
+            SortArray(numbers);
+            int[] addElementInArray = AddElementAtAnyPosition(numbers, 32, numbers.Length);
+            PrintArray(addElementInArray);
+            int[]removeElementInArray = RemoveElementAtPositon(numbers, 4);
+            PrintArray(removeElementInArray);
             LoginInfo("KIN", "21lome");
 
-
+        }
+        // Print  out array element
+       private static void PrintArray(int[] array)
+        {
+            foreach(int number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
         }
 
-        private static void SortArray(int[] numbers)
+        // sorting an array
+       private static int[] SortArray(int[] numbers)
         {
             int temp = 0;
             for (int i = 0; i < numbers.Length - 1; i++)
             {
+                ;
                 for (int j = i + 1; j < numbers.Length; j++)
                 {
                     if (numbers[i] > numbers[j])
@@ -34,60 +42,12 @@ namespace Homework2
                     }
                 }
             }
-            Console.WriteLine("Sorted array");
-            foreach (int number in numbers)
-            {
-
-                Console.Write(number + " ");
-            }
+            return numbers;
+           
         }
-        private static void AddElementAtEnd(int[] numbers, int newNumber)
-        {
-            int[] newNumbers = new int[numbers.Length + 1];
-            int index = numbers.Length;
-            for (int i = 0; i < numbers.Length + 1; i++)
-            {
-                if (i == index)
-                {
-                    newNumbers[i] = newNumber;
-                }
-                else
-                {
-                    newNumbers[i] = numbers[i];
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine("Add element at the end of array");
-            for (int i = 0; i < numbers.Length + 1; i++)
-            {
-                Console.Write(newNumbers[i] + " ");
-            }
-        }
-
-        private static void AddElementAtStart(int[] numbers, int newNumber)
-        {
-            int[] newNumbers = new int[numbers.Length + 1];
-            int index = 0;
-            for (int i = 0; i < numbers.Length + 1; i++)
-            {
-                if (i == index)
-                {
-                    newNumbers[i] = newNumber;
-                }
-                else
-                {
-                    newNumbers[i] = numbers[i - 1];
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine("Add element at the beginning of array");
-            for (int i = 0; i < numbers.Length + 1; i++)
-            {
-                Console.Write(newNumbers[i] + " ");
-            }
-        }
-
-        private static void AddElementAtAnyPosition(int[] numbers, int newNumber, int position)
+       
+        // Add element at any position
+        private static int[] AddElementAtAnyPosition(int[] numbers, int newNumber, int position)
         {
             int[] newNumbers = new int[numbers.Length + 1];
             for (int i = 0; i < numbers.Length + 1; i++)
@@ -105,43 +65,28 @@ namespace Homework2
                     newNumbers[i] = numbers[i - 1];
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine("Add element at the the position you want in the array");
-            for (int i = 0; i < numbers.Length + 1; i++)
-            {
-                Console.Write(newNumbers[i] + " ");
-            }
-        }
-        private static void RemoveElementAtStart(int[] numbers)
-        {
-            int[] newArray = new int[numbers.Length - 1];
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                newArray[i] = numbers[i + 1];
-            }
-            Console.WriteLine();
-            Console.WriteLine("Remove first element in array");
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                Console.Write(newArray[i] + " ");
-            }
+            return newNumbers;
         }
 
-        private static void RemoveElementAtEnd(int[] numbers)
+        // Add element at the beginning
+
+        private static int[] AddElementAtStart(int[] numbers, int newNumber)
         {
-            int[] newArray = new int[numbers.Length - 1];
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                newArray[i] = numbers[i];
-            }
-            Console.WriteLine();
-            Console.WriteLine("Remove last element in array");
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                Console.Write(newArray[i] + " ");
-            }
+            int[] addInTheStart = AddElementAtAnyPosition(numbers, newNumber, 0);
+
+               return addInTheStart;
         }
-        private static void RemoveElementAtPositon(int[] numbers, int index)
+
+        // Add element at the end
+        private static int[] AddElementAtEnd(int[] numbers, int newNumber)
+        {
+            int[] addInTheEnd = AddElementAtAnyPosition(numbers, newNumber, numbers.Length-1);
+
+            return addInTheEnd;
+        }
+
+        // remove element from array at any position
+        private static int[] RemoveElementAtPositon(int[] numbers, int index)
         {
             int[] newArray = new int[numbers.Length - 1];
             for (int i = 0; i < numbers.Length - 1; i++)
@@ -156,14 +101,26 @@ namespace Homework2
                 }
 
             }
-            Console.WriteLine();
-            Console.WriteLine("Remove element at any posiion in array");
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                Console.Write(newArray[i] + " ");
-            }
+            return newArray;
         }
-        static void LoginInfo(string name, string password)
+
+        // Remove element at the Start
+        private static int[] RemoveElementAtStart(int[] numbers)
+        {
+            int[] removeAtTheStart = RemoveElementAtPositon(numbers,0);
+
+            return removeAtTheStart;
+        }
+
+        // Remove element at the End
+        private static int[] RemoveElementAtEnd(int[] numbers)
+        {
+            int[] removeAtTheEnd = RemoveElementAtPositon(numbers, numbers.Length -1);
+
+            return removeAtTheEnd;
+        }
+        // Chech login info
+        private static void LoginInfo(string name, string password)
         {
             string myName = "Kin";
             string pwd = "21Lome";
