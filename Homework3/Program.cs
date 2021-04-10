9a8737d225bfcb76c534cc076c4560697fdcdd09
kindeme <fkounoudji@gmail.com>
2021-04-10 00:59:58 -0500

    Homework3 completed

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -1,12 +1,90 @@
 ﻿using System;
+using System.IO;
 
 namespace Homework3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write(" please enter your username:");
+            string username = Console.ReadLine();
+
+            Console.Write(" please enter your password:");
+            string password = Console.ReadLine();
+            string filePath = @"Users.txt";
+
+            ValidateFile(filePath);
+            LoginAndRegistration(filePath, username, password);
+
+        }
+
+
+
+        static void LoginAndRegistration(string path, string username, string password)
+        {
+            var fileContents = ReadFile(path);
+         
+
+            if (fileContents.Contains(username) && fileContents.Contains(password))
+            {
+                Console.WriteLine("hello!");
+            }
+            else
+            {
+                WriteInTheFile(path, username, password);
+
+                fileContents = ReadFile(path);
+                var contentsArray = fileContents.Split(" ");
+              
+                for(int i=0; i <contentsArray.Length; i++)
+                {
+                    for(int j=i+1; j<contentsArray.Length; j++)
+                    {
+                        if(contentsArray[i] == contentsArray[j])
+                        {
+                            WriteInTheFile(path, username, password);
+                        }
+                    }
+                }
+
+             Console.WriteLine("Register!");
+                
+            }
+
+
+        }
+        static void ValidateFile(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    Environment.Exit(1);
+                }
+            }
+            catch (DuplicateUserCredentialsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        static string ReadFile(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            var contents = reader.ReadToEnd();
+            reader.Dispose();
+            return contents;
+        }
+
+        static void WriteInTheFile(string path, string username, string password)
+        {
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.Write(username + " " +password);
+
+            writer.Dispose();
         }
     }
 }
